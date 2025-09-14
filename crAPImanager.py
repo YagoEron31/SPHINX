c7dfee1cbac8de55a3bb0709e9b945eda5685944
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-09-14 20:24:39 -0300

    Add files via upload

---
+++ crAPImanager.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class ApiManager():
+    BASEURL = "https://api.clashroyale.com/v1"
+    BASEURLPROXY = "https://proxy.royaleapi.dev/v1"
+    def __init__(self, TOKEN):
+        self.headers = {"Authorization": f"Bearer {TOKEN}"}
+    
+
+    def getPlayerBasic(self, playerTag):
+        url = ApiManager.BASEURLPROXY + "/players/" + playerTag.replace("#", "%23")
+        response = requests.get(url, headers=self.headers).json()
+        player = {
+            "name": response["name"],
+            "arena": response["arena"],
+            "wins": response["wins"],
+            "trophies": response["trophies"],
+            "expLevel": response["expLevel"],
+        }
+        return player
+    
+
+    def getPlayer(self, playerTag):
+        url = ApiManager.BASEURLPROXY + "/players/" + playerTag.replace("#", "%23")
+        response = requests.get(url, headers=self.headers).json()
+        return response
+    
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+from crAPImanager import ApiManager
+
+
+app = Flask(__name__)
+
+api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjllMGY1MmZiLTQzZDUtNGMzZS1hNWNmLTY0OTQ2NzIxMzFmOCIsImlhdCI6MTc1Nzg4MTMzOSwic3ViIjoiZGV2ZWxvcGVyL2E3MzM0Yzc5LTkzYWQtYjZlZi0wNDNlLWU3ZDc5NTEyNDFlYyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.2JIVhW5aGUJuq71nqdOm0LV55qPPrvkzlS7TaNfM0OKZAK5UgiB2v_0aG60Il670IfVWxvWZA8tPSl6ORiEPSg"
+api_manager = ApiManager(api_token)
+
+players_tags = [
+    "#2YUCRQPYC",
+    "#LGCL8RVJU",
+    "#RLJ9JP808",
+    "#U2CJLVJUY",
+    "#PRCR808YJ"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/equipe")
+def equipe():
+    return render_template("team.html")
+
+
+@app.route("/players/<type>")
+def players(type):
+    players = {}
+    for tag in players_tags:
+        if type == "basic":
+            player = api_manager.getPlayerBasic(tag)
+        elif type == "full":
+            player = api_manager.getPlayer(tag)
+
+        players[player["name"]] = player
+    return jsonify(players), 200
+
+
+#app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
